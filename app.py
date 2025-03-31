7f05b65f9c9930e07a108550fb9b9664d9b13656
Luciano Alcaraz <Luciano28alcaraz@gmail.com>
2025-03-31 12:03:30 -0300

    Agregue una ruta mas a mi aplicacion web

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/Lucho")
+def apodo():
+    return "<p>LUCHOOO</p>"
+
+@app.route("/Luciano")
+def nombre():
+    return "<p>LUCIANOOO</p>"
